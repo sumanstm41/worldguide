9b0ff8f37fd36ddfad4e68fc9b68847da44f6d8b
sumanstm41 <suman.magar@patientsky.com>
2022-05-10 14:57:25 +0200

    eslint and prettier

--- App.tsx
+++ App.tsx
@@ -1,16 +1,20 @@
-import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { StatusBar, SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  // console.log(StatusBar.currentHeight);
   return (
-    <SafeAreaView style={{ flex: 1, marginTop: 20 }}>
-      <View style={{ padding: 16, backgroundColor: "green" }}>
-        <Text>Search</Text>
-      </View>
-      <View style={{ flex: 1, padding: 16, backgroundColor: "blue" }}>
-        <Text>List</Text>
-      </View>
-    </SafeAreaView>
+    <>
+      <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
+        <View style={{ padding: 16, backgroundColor: "green" }}>
+          <Text>Search</Text>
+        </View>
+        <View style={{ flex: 1, padding: 16, backgroundColor: "blue" }}>
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
+      {/* <StatusBar style="auto" /> */}
+    </>
     // <View style={styles.container}>
     //   <Text>Open up App.tsx to start working on your app!</Text>
     //   <StatusBar style="auto" />
