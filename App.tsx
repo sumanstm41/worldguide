4b660e3134fe8d095bf37839693445080632f109
sumanstm41 <suman.magar@patientsky.com>
2022-05-10 10:28:44 +0200

    Initial Test

--- App.tsx
+++ App.tsx
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1, marginTop: 20 }}>
+      <View style={{ padding: 16, backgroundColor: "green" }}>
+        <Text>Search</Text>
+      </View>
+      <View style={{ flex: 1, padding: 16, backgroundColor: "blue" }}>
+        <Text>List</Text>
+      </View>
+    </SafeAreaView>
+    // <View style={styles.container}>
+    //   <Text>Open up App.tsx to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
